7207c227cdc16289be592b179fac355ba3edacbe
Anna <ds_anna@bk.ru>
2022-11-27 20:03:32 +0300

    Homework1

--- Homework1/Task2.java
+++ Homework1/Task2.java
@@ -1,20 +1,25 @@
 package Homeworks.Homework1;
 
-// Посмотрите на код, и подумайте сколько разных типов исключений вы тут сможете получить?
+import java.util.Arrays;
+
+//Реализуйте метод,принимающий в качестве аргументов два целочисленных массива,
+//и возвращающий новый массив,каждый элемент которого равен разности элементов
+//двух входящих массивов в той же ячейке.
+//Если длины массивов не равны, необходимо как-то оповестить пользователя.
 
 public class Task2 {
     public static void main(String[] args) {
-        System.out.println(sum2d(new String[][]{{"0","1","2","3","4"},{"4","3","2","1","0"}}));
+        System.out.println(Arrays.toString(diffElemArras(new int[]{1, 2, 3}, new int[]{3, 2, 1})));
     }
 
-    public static int sum2d(String[][] arr) {
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < 5; j++) {
-                int val = Integer.parseInt(arr[i][j]);
-                sum += val;
-            }
+    public static int[] diffElemArras(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length) {
+            System.out.println("Arrays are not equal!");
         }
-        return sum;
+        int[] result = new int[arr1.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = arr1[i] - arr2[i];
+        }
+        return result;
     }
 }
--- Homework1/Task3.java
+++ Homework1/Task3.java
@@ -1,25 +1,41 @@
 package Homeworks.Homework1;
 
 import java.util.Arrays;
+import java.util.Random;
 
-//Реализуйте метод,принимающий в качестве аргументов два целочисленных массива,
-//и возвращающий новый массив,каждый элемент которого равен разности элементов
-//двух входящих массивов в той же ячейке.
-//Если длины массивов не равны, необходимо как-то оповестить пользователя.
+//Реализуйте метод, принимающий в качестве аргументов два целочисленных
+// массива, и возвращающий новый массив, каждый элемент которого равен частному элементов двух входящих массивов в той
+// же ячейке. Если длины массивов не равны, необходимо как-то оповестить пользователя. 
+// Важно: При выполнении метода единственное исключение, которое пользователь может увидеть - RuntimeException, т.е. ваше.
 
 public class Task3 {
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(diffElemArras(new int[]{1, 2, 3}, new int[]{3, 2, 1})));
+
+    public static double[] arrayElemDiv(int[] arrayFirst, int[] arraySecond) {
+        if (arrayFirst.length != arraySecond.length)
+            throw new RuntimeException("Arrays are not equal!");
+        double[] arrayDiv = new double[arrayFirst.length];
+        for (int i = 0; i < arrayDiv.length; i++) {
+            if (arraySecond[i] == 0) {
+                throw new RuntimeException("Devision by zero");
+            }
+            arrayDiv[i] = arrayFirst[i] / arraySecond[i] / 1.0;
+        }
+        return arrayDiv;
     }
 
-    public static int[] diffElemArras(int[] arr1, int[] arr2) {
-        if (arr1.length != arr2.length) {
-            System.out.println("Arrays are not equal!");
+    public static void main(String[] args) {
+        // int [] arrOne = new int [new Random().nextInt(5)];
+        int[] arrOne = new int[4];
+        for (int index = 0; index < arrOne.length; index++) {
+            arrOne[index] = new Random().nextInt(10);
         }
-        int[] result = new int[arr1.length];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = arr1[i] - arr2[i];
+        // int [] arrTwo = new int [new Random().nextInt(5)];
+        int[] arrTwo = new int[4];
+        for (int i = 0; i < arrTwo.length; i++) {
+            arrTwo[i] = new Random().nextInt(10);
         }
-        return result;
+        double[] arrDivider = arrayElemDiv(arrOne, arrTwo);
+        System.out.println(Arrays.toString(arrDivider));
+
     }
 }
